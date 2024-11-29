1ca0615d5b5dda51219c7301a2cf99e7530927b5
Krampus B. <tiobarryx@gmail.com>
2024-11-29 11:23:19 +0100

    Actualizar database.js

--- src/utils/database.js
+++ src/utils/database.js
@@ -10,160 +10,162 @@ const ANTI_LINK_GROUPS_FILE = "anti-link-groups";
 const DELETED_MESSAGES_FILE = "deleted-messages";
 
 function createIfNotExists(fullPath) {
-if (!fs.existsSync(fullPath)) {
-fs.writeFileSync(fullPath, JSON.stringify([]));
-}
+  if (!fs.existsSync(fullPath)) {
+    fs.writeFileSync(fullPath, JSON.stringify([]));
+  }
 }
 
 function readJSON(jsonFile) {
-const fullPath = path.resolve(databasePath, `${jsonFile}.json`);
-createIfNotExists(fullPath);
-return JSON.parse(fs.readFileSync(fullPath, "utf8"));
+  const fullPath = path.resolve(databasePath, `${jsonFile}.json`);
+  createIfNotExists(fullPath);
+  return JSON.parse(fs.readFileSync(fullPath, "utf8"));
 }
 
 function writeJSON(jsonFile, data) {
-const fullPath = path.resolve(databasePath, `${jsonFile}.json`);
-createIfNotExists(fullPath);
-fs.writeFileSync(fullPath, JSON.stringify(data));
+  const fullPath = path.resolve(databasePath, `${jsonFile}.json`);
+  createIfNotExists(fullPath);
+  fs.writeFileSync(fullPath, JSON.stringify(data));
 }
 
-// Función para añadir un mensaje borrado
 exports.addDeletedMessage = (groupId, userId, messageText) => {
-  const filename = DELETED_MESSAGES_FILE;
-  const deletedMessages = readJSON(filename);
+  const deletedMessages = readJSON(DELETED_MESSAGES_FILE);
   deletedMessages.push({ groupId, userId, messageText, timestamp: new Date().toISOString() });
+
   if (deletedMessages.length > 100) {
     deletedMessages.shift();
   }
-  writeJSON(filename, deletedMessages);
+
+  writeJSON(DELETED_MESSAGES_FILE, deletedMessages);
 };
 
-// Función para obtener los últimos 6 mensajes borrados
 exports.getLastDeletedMessages = (groupId) => {
-const filename = DELETED_MESSAGES_FILE;
-const deletedMessages = readJSON(filename);
-
-// Filtrar mensajes por grupo y devolver los últimos 6
-return deletedMessages
-.filter((message) => message.groupId === groupId)
-.slice(-6)
-.reverse();
+  const deletedMessages = readJSON(DELETED_MESSAGES_FILE);
+  return deletedMessages
+    .filter((message) => message.groupId === groupId)
+    .slice(-6)
+    .reverse();
 };
 
 exports.activateGroup = (groupId) => {
-const filename = INACTIVE_GROUPS_FILE;
-const inactiveGroups = readJSON(filename);
-const index = inactiveGroups.indexOf(groupId);
-if (index === -1) {
-return;
-}
-inactiveGroups.splice(index, 1);
-writeJSON(filename, inactiveGroups);
+  const inactiveGroups = readJSON(INACTIVE_GROUPS_FILE);
+  const index = inactiveGroups.indexOf(groupId);
+
+  if (index !== -1) {
+    inactiveGroups.splice(index, 1);
+    writeJSON(INACTIVE_GROUPS_FILE, inactiveGroups);
+  }
 };
 
 exports.deactivateGroup = (groupId) => {
-const filename = INACTIVE_GROUPS_FILE;
-const inactiveGroups = readJSON(filename);
-if (!inactiveGroups.includes(groupId)) {
-inactiveGroups.push(groupId);
-}
-writeJSON(filename, inactiveGroups);
+  const inactiveGroups = readJSON(INACTIVE_GROUPS_FILE);
+  if (!inactiveGroups.includes(groupId)) {
+    inactiveGroups.push(groupId);
+    writeJSON(INACTIVE_GROUPS_FILE, inactiveGroups);
+  }
 };
 
 exports.isActiveGroup = (groupId) => {
-const filename = INACTIVE_GROUPS_FILE;
-const inactiveGroups = readJSON(filename);
-return !inactiveGroups.includes(groupId);
+  const inactiveGroups = readJSON(INACTIVE_GROUPS_FILE);
+  return !inactiveGroups.includes(groupId);
 };
 
 exports.activateWelcomeGroup = (groupId) => {
-const filename = NOT_WELCOME_GROUPS_FILE;
-const notWelcomeGroups = readJSON(filename);
-const index = notWelcomeGroups.indexOf(groupId);
-if (index === -1) {
-return;
-}
-notWelcomeGroups.splice(index, 1);
-writeJSON(filename, notWelcomeGroups);
+  const notWelcomeGroups = readJSON(NOT_WELCOME_GROUPS_FILE);
+  const index = notWelcomeGroups.indexOf(groupId);
+
+  if (index !== -1) {
+    notWelcomeGroups.splice(index, 1);
+    writeJSON(NOT_WELCOME_GROUPS_FILE, notWelcomeGroups);
+  }
 };
 
 exports.deactivateWelcomeGroup = (groupId) => {
-const filename = NOT_WELCOME_GROUPS_FILE;
-const notWelcomeGroups = readJSON(filename);
-if (!notWelcomeGroups.includes(groupId)) {
-notWelcomeGroups.push(groupId);
-}
-writeJSON(filename, notWelcomeGroups);
+  const notWelcomeGroups = readJSON(NOT_WELCOME_GROUPS_FILE);
+  if (!notWelcomeGroups.includes(groupId)) {
+    notWelcomeGroups.push(groupId);
+    writeJSON(NOT_WELCOME_GROUPS_FILE, notWelcomeGroups);
+  }
 };
 
 exports.isActiveWelcomeGroup = (groupId) => {
-const filename = NOT_WELCOME_GROUPS_FILE;
-const notWelcomeGroups = readJSON(filename);
-return !notWelcomeGroups.includes(groupId);
+  const notWelcomeGroups = readJSON(NOT_WELCOME_GROUPS_FILE);
+  return !notWelcomeGroups.includes(groupId);
 };
 
 exports.getAutoResponderResponse = (match) => {
-const filename = "auto-responder";
-const responses = readJSON(filename);
-const matchUpperCase = match.toLocaleUpperCase();
-const data = responses.find((response) => response.match.toLocaleUpperCase() === matchUpperCase);
-if (!data) {
-return null;
-}
-return data.answer;
+  const responses = readJSON("auto-responder");
+  const matchUpperCase = match.toLocaleUpperCase();
+  const data = responses.find((response) => response.match.toLocaleUpperCase() === matchUpperCase);
+  return data ? data.answer : null;
 };
 
 exports.activateAutoResponderGroup = (groupId) => {
-const filename = INACTIVE_AUTO_RESPONDER_GROUPS_FILE;
-const inactiveAutoResponderGroups = readJSON(filename);
-const index = inactiveAutoResponderGroups.indexOf(groupId);
-if (index === -1) {
-return;
-}
-inactiveAutoResponderGroups.splice(index, 1);
-writeJSON(filename, inactiveAutoResponderGroups);
+  const inactiveAutoResponderGroups = readJSON(INACTIVE_AUTO_RESPONDER_GROUPS_FILE);
+  const index = inactiveAutoResponderGroups.indexOf(groupId);
+
+  if (index !== -1) {
+    inactiveAutoResponderGroups.splice(index, 1);
+    writeJSON(INACTIVE_AUTO_RESPONDER_GROUPS_FILE, inactiveAutoResponderGroups);
+  }
 };
 
 exports.deactivateAutoResponderGroup = (groupId) => {
-const filename = INACTIVE_AUTO_RESPONDER_GROUPS_FILE;
-const inactiveAutoResponderGroups = readJSON(filename);
+  const inactiveAutoResponderGroups = readJSON(INACTIVE_AUTO_RESPONDER_GROUPS_FILE);
 
-if (!inactiveAutoResponderGroups.includes(groupId)) {
-inactiveAutoResponderGroups.push(groupId);
-}
-writeJSON(filename, inactiveAutoResponderGroups);
+  if (!inactiveAutoResponderGroups.includes(groupId)) {
+    inactiveAutoResponderGroups.push(groupId);
+    writeJSON(INACTIVE_AUTO_RESPONDER_GROUPS_FILE, inactiveAutoResponderGroups);
+  }
 };
 
 exports.isActiveAutoResponderGroup = (groupId) => {
-const filename = INACTIVE_AUTO_RESPONDER_GROUPS_FILE;
-const inactiveAutoResponderGroups = readJSON(filename);
-return !inactiveAutoResponderGroups.includes(groupId);
+  const inactiveAutoResponderGroups = readJSON(INACTIVE_AUTO_RESPONDER_GROUPS_FILE);
+  return !inactiveAutoResponderGroups.includes(groupId);
 };
 
 exports.activateAntiLinkGroup = (groupId) => {
-const filename = ANTI_LINK_GROUPS_FILE;
-const antiLinkGroups = readJSON(filename);
-if (!antiLinkGroups.includes(groupId)) {
-antiLinkGroups.push(groupId);
-}
-writeJSON(filename, antiLinkGroups);
+  const antiLinkGroups = readJSON(ANTI_LINK_GROUPS_FILE);
+  if (!antiLinkGroups.includes(groupId)) {
+    antiLinkGroups.push(groupId);
+    writeJSON(ANTI_LINK_GROUPS_FILE, antiLinkGroups);
+  }
 };
 
 exports.deactivateAntiLinkGroup = (groupId) => {
-const filename = ANTI_LINK_GROUPS_FILE;
-const antiLinkGroups = readJSON(filename);
-const index = antiLinkGroups.indexOf(groupId);
-if (index === -1) {
-return;
-}
-antiLinkGroups.splice(index, 1);
-writeJSON(filename, antiLinkGroups);
+  const antiLinkGroups = readJSON(ANTI_LINK_GROUPS_FILE);
+  const index = antiLinkGroups.indexOf(groupId);
+
+  if (index !== -1) {
+    antiLinkGroups.splice(index, 1);
+    writeJSON(ANTI_LINK_GROUPS_FILE, antiLinkGroups);
+  }
 };
 
 exports.isActiveAntiLinkGroup = (groupId) => {
-const filename = ANTI_LINK_GROUPS_FILE;
-const antiLinkGroups = readJSON(filename);
-return antiLinkGroups.includes(groupId);
+  const antiLinkGroups = readJSON(ANTI_LINK_GROUPS_FILE);
+  return antiLinkGroups.includes(groupId);
 };
 
 module.exports = exports;
+exports.activateAntiFloodGroup = (groupId) => {
+  const antiFloodGroups = readJSON("anti-flood-groups");
+  if (!antiFloodGroups.includes(groupId)) {
+    antiFloodGroups.push(groupId);
+    writeJSON("anti-flood-groups", antiFloodGroups);
+  }
+};
+
+exports.deactivateAntiFloodGroup = (groupId) => {
+  const antiFloodGroups = readJSON("anti-flood-groups");
+  const index = antiFloodGroups.indexOf(groupId);
+
+  if (index !== -1) {
+    antiFloodGroups.splice(index, 1);
+    writeJSON("anti-flood-groups", antiFloodGroups);
+  }
+};
+
+exports.isActiveAntiFloodGroup = (groupId) => {
+  const antiFloodGroups = readJSON("anti-flood-groups");
+  return antiFloodGroups.includes(groupId);
+};
