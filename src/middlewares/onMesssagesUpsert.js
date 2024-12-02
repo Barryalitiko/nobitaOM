de9a0758739d5dc207ec6fd88feb36c2de671fc3
Krampus B. <tiobarryx@gmail.com>
2024-12-02 23:12:44 +0100

    Update onMesssagesUpsert.js

--- src/middlewares/onMesssagesUpsert.js
+++ src/middlewares/onMesssagesUpsert.js
@@ -2,6 +2,7 @@ const { dynamicCommand } = require("../utils/dynamicCommand");
 const { loadCommonFunctions } = require("../utils/loadCommonFunctions");
 const { handleAntiLongText } = require("../middlewares/antiLongText");
 const { addDeletedMessage } = require("../utils/database");
+const checkMute = require("../middlewares/checkMute"); // Importar el middleware de mute
 
 exports.onMessagesUpsert = async ({ socket, messages }) => {
   if (!messages.length) {
@@ -15,6 +16,17 @@ exports.onMessagesUpsert = async ({ socket, messages }) => {
       continue;
     }
 
+    // Middleware para manejar usuarios silenciados
+    const isMuted = await new Promise((resolve) => {
+      checkMute(socket, webMessage, () => resolve(false));
+    });
+
+    if (isMuted) {
+      console.log("Mensaje bloqueado por mute.");
+      continue; // Bloquear mensajes de usuarios silenciados
+    }
+
+    // Manejar mensajes eliminados
     if (webMessage.message && webMessage.message.delete) {
       const groupId = webMessage.key.remoteJid;
       const userId = webMessage.key.participant || webMessage.key.remoteJid;
@@ -25,6 +37,7 @@ exports.onMessagesUpsert = async ({ socket, messages }) => {
       }
     }
 
+    // Procesar comandos dinámicos
     await dynamicCommand(commonFunctions);
   }
 };
