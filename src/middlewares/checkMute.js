00a6c0253812833708146a99285aeb5653aeb9ee
Krampus B. <tiobarryx@gmail.com>
2024-12-03 15:28:52 +0100

    Actualizar checkMute.js

--- src/middlewares/checkMute.js
+++ src/middlewares/checkMute.js
@@ -1,24 +1,14 @@
 const { isUserMuted } = require("../utils/database");
 
-module.exports = async (socket, webMessage, next) => {
+exports.checkMute = async ({ socket, userJid, remoteJid }) => {
   try {
-    const groupId = webMessage.key.remoteJid; // ID del grupo
-    const userJid = webMessage.key.participant || webMessage.key.remoteJid; // JID del usuario que envió el mensaje
-
-    // Verificar si es un grupo
-    if (!groupId.endsWith("@g.us")) {
-      return next(); // Si no es un grupo, continuar con el flujo normal
+    const { participants } = await socket.groupMetadata(remoteJid);
+    const participant = participants.find((participant) => (link unavailable) === userJid);
+    if (!participant) {
+      return false;
     }
-
-    // Verificar si el usuario está silenciado
-    if (isUserMuted(groupId, userJid)) {
-      console.log(`Usuario silenciado detectado: ${userJid} en el grupo ${groupId}. Bloqueando mensaje.`);
-      return; // Bloquear mensaje (no pasar al siguiente middleware)
-    }
-
-    next(); // Continuar con el flujo normal si no está silenciado
+    return isUserMuted(remoteJid, userJid);
   } catch (error) {
-    console.error("Error en el middleware de mute:", error);
-    next(); // Asegurarse de no detener el flujo en caso de error
+    return false;
   }
 };
